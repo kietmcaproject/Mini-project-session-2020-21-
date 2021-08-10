7a69f0b43943831cf13c4aa2a24e3cd568bef479
Anupam Shukla <65623658+anupamshukla1@users.noreply.github.com>
2021-08-10 16:15:29 +0530

    initial
    This program is about end of file

---
+++ end_of_file.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class end_of_file {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 1;
+        while (sc.hasNext()) {
+            System.out.println(i++ + " " + sc.nextLine());
+        }
+    }
+}
